692822d66dda17cad968a10acca9012e24b4843c
sjlee <szinlee@gmail.com>
2020-10-18 23:00:25 +0900

    1221. Split a String in Balanced Strings

--- src/main/leetcode/easy/no771/JewelsAndStones.java
+++ src/main/leetcode/easy/no771/JewelsAndStones.java
@@ -4,7 +4,6 @@ public class JewelsAndStones {
     public int numJewelsInStones(String J, String S) {
     	int jewel_count = 0;
     	for(int i=0; i<J.length(); i++) {
-    		System.out.println(J.charAt(i));
     		for(int j=0; j<S.length(); j++) {
     			if(J.charAt(i) == S.charAt(j)) {
     				jewel_count++;
